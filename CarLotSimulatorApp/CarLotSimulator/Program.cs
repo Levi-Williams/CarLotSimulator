5ec75f26d5d6ac5be34a6fc3f325a2db7cf479f2
Levi Williams <ljw758@hotmail.com>
2023-03-08 21:57:32 -0600

    Completed Exercise

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -28,6 +29,48 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            //START CODE//
+
+            List<Car> Cars = new List<Car>();
+
+            //Car 1//
+
+            Car chevy= new Car();
+            chevy.Make = "Chevrolet";
+            chevy.Year = 2016;
+            chevy.Model = "Malibu";
+            chevy.EngineNoise = "Burr";
+            chevy.HonkNoise = "high beep";
+
+            chevy.MakeEngineNoise(chevy.EngineNoise);
+            chevy.MakeHonkNoise(chevy.HonkNoise);
+
+            Cars.Add(chevy);
+
+            //Car 2//
+
+            Car ford = new Car() { Make = "Ford", Year = 2015, Model = "Focus", EngineNoise = "Clank", HonkNoise = "boop" };
+
+            ford.MakeEngineNoise(ford.EngineNoise);
+
+            ford.MakeHonkNoise(ford.HonkNoise);
+
+            Cars.Add(ford);
+
+            //Car 3//
+
+            Car toyota = new Car(2020, "Toyota", "Corolla", "clunk", "bleep", true);
+
+            toyota.MakeHonkNoise(toyota.HonkNoise);
+
+            toyota.MakeEngineNoise(toyota.EngineNoise);
+
+            Cars.Add(toyota);
+
+
+            foreach(var Car in Cars) { Console.WriteLine($" Make: {Car.Make}, Year: {Car.Year}, Model: {Car.Model}, Engine Noise Description: {Car.EngineNoise}, Honk Noise Description: {Car.HonkNoise} "); }
+
         }
     }
 }
